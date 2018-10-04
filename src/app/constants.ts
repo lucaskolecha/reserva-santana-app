63088d413ea746c32169ae573241b1e4ccad1b1e
lucaskolecha <lkolecha@hotmail.com>
2018-10-04 20:52:23 -0300

    Listando estabelicimentos

---
+++ src/app/constants.ts
@@ -0,0 +1,6 @@
+export const Constants = {
+    COLLECTION_APARTMENTS: 'apartments',
+    COLLECTION_SYNDICATES: 'syndicates',
+    COLLECTION_COMPANIES: 'companies',
+    COLLECTION_PRODUCTS: 'products',
+  }
---
+++ src/providers/company/company.ts
@@ -0,0 +1,40 @@
+import { Injectable } from '@angular/core';
+import { firestore } from 'firebase';
+import { Constants } from '../../app/constants';
+
+/*
+  Generated class for the CompanyProvider provider.
+
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class CompanyProvider {
+
+  private db;
+
+  constructor() {
+    this.db = firestore();
+    this.db.settings({ timestampsInSnapshots: true });
+  }
+
+  getAll() {
+    let all = [];
+    return new Promise((response) => {
+      this.db.collection(Constants.COLLECTION_COMPANIES).get().then((documents) => {
+        documents.forEach(function (doc) {
+          let item = {
+            name: doc.data().name,
+            phone: doc.data().phone,
+            image: doc.data().image,
+            email: doc.data().email,
+            uid: doc.id
+          }
+          all.push(item);
+        });
+        response(all);
+      });
+    });
+  }
+
+}
